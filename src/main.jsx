0453cf1bb2a7d3ffc8e282185432401a09dd9acc
BeerWalkAlone <sinnakhon@hotmail.com>
2023-04-29 17:08:47 +0700

    yah

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Index from './routes'
 import './index.css'
-window.$api = "https://backend-exconox-project.vercel.app/"
+window.$api = "https://backend-exconox-project.vercel.app"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Index/>
